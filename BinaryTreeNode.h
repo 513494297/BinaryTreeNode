f9fe31ef62d924a49302bb218cc2b8356b58199d
huafangT <513494297@qq.com>
2017-12-27 15:27:32 +0800

    BinaryTree

--- BinaryTreeNode.h
+++ BinaryTreeNode.h
@@ -86,42 +86,4 @@
  }];
  NSLog(@"层次遍历结果：%@", [orderArray3 componentsJoinedByString:@","]);
  
- 
- NSArray *arr = [NSArray arrayWithObjects:@(7),@(6),@(3),@(2),@(1),@(9),@(10),@(12),@(14),@(4),@(14), nil];
- BinaryTreeNode *tree = [BinaryTreeNode new];
- tree = [BinaryTreeNode createTreeWithValues:arr];
- 
- BinaryTreeNode *tree1 = [BinaryTreeNode treeNodeAtIndex:3 inTree:tree];
- NSLog(@"%@",tree1);
- 
- NSMutableArray *orderArray = [NSMutableArray array];
- [BinaryTreeNode preOrderTraverseTree:tree handler:^(BinaryTreeNode *treeNode) {
- [orderArray addObject:@(treeNode.value)];
- }];
- NSLog(@"先序遍历结果：%@", [orderArray componentsJoinedByString:@","]);
- 
- NSMutableArray *orderArray1 = [NSMutableArray array];
- [BinaryTreeNode inOrderTraverseTree:tree handler:^(BinaryTreeNode *treeNode) {
- 
- [orderArray1 addObject:@(treeNode.value)];
- 
- }];
- NSLog(@"中序遍历结果：%@", [orderArray1 componentsJoinedByString:@","]);
- 
- 
- NSMutableArray *orderArray2 = [NSMutableArray array];
- [BinaryTreeNode postOrderTraverseTree:tree handler:^(BinaryTreeNode *treeNode) {
- [orderArray2 addObject:@(treeNode.value)];
- 
- }];
- NSLog(@"后序遍历结果：%@", [orderArray2 componentsJoinedByString:@","]);
- 
- 
- NSMutableArray *orderArray3 = [NSMutableArray array];
- [BinaryTreeNode levelTraverseTree:tree handler:^(BinaryTreeNode *treeNode) {
- [orderArray3 addObject:@(treeNode.value)];
- 
- }];
- NSLog(@"层次遍历结果：%@", [orderArray3 componentsJoinedByString:@","]);
- 
  */
